015686e301682dd76e6605e00bdfadb482665552
JoJavlon Kayumov <javlon20048@gmail.com>
2022-01-16 18:44:00 +0500

    implementing a folder upload button

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import path
+from django.shortcuts import render
 
 # Register your models here.
 from .models import User, Surah, Reciter
@@ -11,7 +13,16 @@ class UserAdmin(admin.ModelAdmin):
     admin.site.site_header = "Quran Bot Admin"
     admin.site.site_title = "Admin"
 
-
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path('upload-folder/', self.upload_folder),]
+        return new_urls + urls
+    
+    def upload_folder(self, request):
+        return render(request, "admin/folder_upload.html")
+    
+    
+    
 admin.site.register(User, UserAdmin)
 admin.site.register(Surah)
 admin.site.register(Reciter)
